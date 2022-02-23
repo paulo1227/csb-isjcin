071f12a10e6069a5620b3a24d86ec2359ca43ee8
Code Sandbox <hello@codesandbox.io>
2022-02-23 20:32:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cfd92fd699d8725f2ec92b3ea8e56f02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c0b985a9ef774beab841007571a285",
+    "url": "https://.github.io/csb-isjcin/index.html"
+  },
+  {
+    "revision": "58a4a3a718eeb9f182da",
+    "url": "https://.github.io/csb-isjcin/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "3c2968ef8b2eb466d66b",
+    "url": "https://.github.io/csb-isjcin/static/js/2.32bdae52.chunk.js"
+  },
+  {
+    "revision": "58a4a3a718eeb9f182da",
+    "url": "https://.github.io/csb-isjcin/static/js/main.b741b1b2.chunk.js"
+  },
+  {
+    "revision": "2b9f4cb046913aedf351",
+    "url": "https://.github.io/csb-isjcin/static/js/runtime~main.30aca255.js"
+  }
+]);
---
+++ static/js/main.b741b1b2.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),r=t(2),c=t.n(r);t(8);function l(){return o.a.createElement("div",{className:"App"},o.a.createElement("h1",null,"Hello CodeSandbox"),o.a.createElement("h2",null,"Start editing to see some magic happen!"))}var i=document.getElementById("root");c.a.render(o.a.createElement(l,null),i)}},[[3,1,2]]]);
+//# sourceMappingURL=main.b741b1b2.chunk.js.map
